24c5302f8044713875c4aa2c4cb48782adb331f2
schminitz <gillian@affinitic.be>
2015-09-28 14:25:20 +0200

    Not persistant menu depends on cpskin parameter refs #6267

--- cpskin/menu/browser/resources/cpskin_menu.js
+++ cpskin/menu/browser/resources/cpskin_menu.js
@@ -22,15 +22,4 @@ $( document ).ready(function() {
             return false;
         })
     });
-
-    // Click outside of the menu
-    $(document).click(function(event) {
-        if(!$(event.target).closest('ul.sf-menu').length) {
-            if($('ul.sf-menu').is(":visible")) {
-                $('ul.sf-menu').each(function(){
-                    $(this).hide();
-                });
-            }
-        }
-    })
 });
