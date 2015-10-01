88b2c60ace7f0f916b3f6101ea28c905da799bfc
Benoît Suttor <benoit.suttor@imio.be>
2015-10-01 09:45:44 +0200

    Unittest for menu now use memcache instead ramcache

--- cpskin/menu/tests/test_menu.py
+++ cpskin/menu/tests/test_menu.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest2 as unittest
-from zope.component import getUtility, queryUtility
-from zope.ramcache.interfaces.ram import IRAMCache
+from zope.component import queryUtility
 from plone.uuid.interfaces import IUUID
 from plone import api
 from cpskin.menu.testing import CPSKIN_MENU_INTEGRATION_TESTING
@@ -16,11 +15,6 @@ def cache_exist(viewlet):
     adapter = queryUtility(ICacheChooser)('cpskin.menu.browser.menu.superfish_portal_tabs')
     key = cache_key_desktop(None, viewlet)
     return adapter.get(key) and True or False
-
-
-def get_cache_miss(viewlet):
-    storage = getUtility(IRAMCache)._getStorage()
-    return storage._misses.get('cpskin.menu.browser.menu.superfish_portal_tabs', 0)
 
 
 def empty_cache():
@@ -45,31 +39,31 @@ class TestMenu(unittest.TestCase):
         viewlet.update()
         self.assertTrue(viewlet.is_homepage)
         menus = viewlet.superfish_portal_tabs()
-        self.assertEqual(len(menus), 2)
+        self.assertEqual(len(menus), 7866)
 
     def test_menu_cache_key_on_root(self):
         viewlet = CpskinMenuViewlet(self.portal, self.request, None, None)
         viewlet.update()
-        key = cache_key(viewlet.superfish_portal_tabs, viewlet)
-        self.assertTrue(key.startswith('menu.'))
-        self.assertTrue(key.endswith(IUUID(self.portal)))
+        key = cache_key_desktop(viewlet.superfish_portal_tabs, viewlet)
+        self.assertTrue(key.startswith('menu-'))
+        self.assertTrue(key.endswith(IUUID(viewlet._get_real_context())))
 
     def test_menu_cache_key_on_communes(self):
         communes = getattr(self.portal, 'commune')
         viewlet = CpskinMenuViewlet(communes, self.request, None, None)
         viewlet.update()
-        key = cache_key(viewlet.superfish_portal_tabs, viewlet)
-        self.assertTrue(key.startswith('menu.'))
-        self.assertTrue(key.endswith(IUUID(self.portal)))
+        key = cache_key_desktop(viewlet.superfish_portal_tabs, viewlet)
+        self.assertTrue(key.startswith('menu-'))
+        self.assertTrue(key.endswith(IUUID(viewlet._get_real_context())))
 
     def test_menu_cache_key_on_communes_subitem(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
-        communes = getattr(self.portal, 'commune')
+        # communes = getattr(self.portal, 'commune')
         viewlet = CpskinMenuViewlet(item, self.request, None, None)
         viewlet.update()
-        key = cache_key(viewlet.superfish_portal_tabs, viewlet)
-        self.assertTrue(key.startswith('menu.'))
-        self.assertTrue(key.endswith(IUUID(self.portal)))
+        key = cache_key_desktop(viewlet.superfish_portal_tabs, viewlet)
+        self.assertTrue(key.startswith('menu-'))
+        self.assertTrue(key.endswith(IUUID(viewlet._get_real_context())))
 
     def test_menu_cache_usage_test_fail(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
@@ -108,10 +102,6 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
         self.assertEqual(cache_exist(viewlet), True)
-        # XXX l'invalidation ne se fait pas. je ne comprends pas pourquoi.
-        # CF jfroche, peut etre un probleme avec le notify(event) ?
-#         api.content.move(item, self.portal)
-        # Le move() lance bien invalidate_menu, mais celui ci ne fonctionne pas
         invalidate_menu(item)
         self.assertEqual(cache_exist(viewlet), False)
 
@@ -121,73 +111,71 @@ class TestMenu(unittest.TestCase):
         viewlet = CpskinMenuViewlet(item, self.request, None, None)
         viewlet.update()
 
-#         self.assertEqual(cache_exist(viewlet), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(viewlet), 1)
+        self.assertEqual(cache_exist(viewlet), True)
 
         loisirs = self.portal.restrictedTraverse('loisirs')
         invalidate_menu(loisirs)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(viewlet), 2)
+        self.assertEqual(cache_exist(viewlet), True)
 
-        invalidate_menu(commune)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(viewlet), 3)
+        invalidate_menu(commune)
+        self.assertEqual(cache_exist(viewlet), False)
 
     def test_objet_modification_invalidates_menu(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
         viewlet = CpskinMenuViewlet(item, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         item.setTitle('Test Cache Invalidation')
         item.processForm()
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), True)
 
     def test_object_creation_invalidates_menu(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
         viewlet = CpskinMenuViewlet(item, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         api.content.create(item, 'Folder', 'foo')
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), False)
 
     def test_object_publication_invalidates_menu(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
         viewlet = CpskinMenuViewlet(item, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         api.content.transition(item, 'publish')
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), True)
 
     def test_object_removed_invalidates_menu(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
         commune = self.portal.restrictedTraverse('commune')
         viewlet = CpskinMenuViewlet(commune, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         api.content.delete(item)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.update()
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), True)
 
     def test_object_moved_invalidates_menus(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
@@ -195,38 +183,39 @@ class TestMenu(unittest.TestCase):
         loisirs = self.portal.restrictedTraverse('loisirs')
         viewlet = CpskinMenuViewlet(commune, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         viewlet_loisirs = CpskinMenuViewlet(loisirs, self.request, None, None)
         viewlet_loisirs.update()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
+        self.assertEqual(cache_exist(viewlet_loisirs), False)
         viewlet_loisirs.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
-        viewlet_loisirs.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
+        self.assertEqual(cache_exist(viewlet_loisirs), True)
         api.content.move(item, loisirs)
+        self.assertEqual(cache_exist(viewlet), False)
+        self.assertEqual(cache_exist(viewlet_loisirs), False)
         viewlet.update()
         viewlet_loisirs.update()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), False)
+        self.assertEqual(cache_exist(viewlet_loisirs), False)
         viewlet_loisirs.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
-        viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), False)
+        self.assertEqual(cache_exist(viewlet_loisirs), True)
 
     def test_object_rename_invalidates_menu(self):
         item = self.portal.restrictedTraverse('commune/services_communaux')
         commune = self.portal.restrictedTraverse('commune')
         viewlet = CpskinMenuViewlet(commune, self.request, None, None)
         viewlet.update()
-        self.assertEqual(cache_exist(), 0)
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 1)
+        self.assertEqual(cache_exist(viewlet), True)
         api.content.rename(item, new_id='sc')
         viewlet.update()
+        self.assertEqual(cache_exist(viewlet), False)
         viewlet.superfish_portal_tabs()
-        self.assertEqual(cache_exist(), 2)
+        self.assertEqual(cache_exist(viewlet), True)
