f3d045ce45fffc453e19789e689a0cdc1f24b175
Benoît Suttor <benoit.suttor@imio.be>
2015-10-27 11:15:07 +0100

    Fix test lenght menus

--- cpskin/menu/tests/test_menu.py
+++ cpskin/menu/tests/test_menu.py
@@ -39,7 +39,7 @@ class TestMenu(unittest.TestCase):
         viewlet.update()
         self.assertTrue(viewlet.is_homepage)
         menus = viewlet.superfish_portal_tabs()
-        self.assertEqual(len(menus), 7866)
+        self.assertEqual(len(menus), 4563)
 
     def test_menu_cache_key_on_root(self):
         viewlet = CpskinMenuViewlet(self.portal, self.request, None, None)
