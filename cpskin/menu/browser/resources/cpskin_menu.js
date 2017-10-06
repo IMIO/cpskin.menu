c5e1f1608ba67c7a5305392065c0cf7bbaa5afe2
Godefroid Chapelle <gotcha@bubblenet.be>
2017-10-06 09:51:07 +0200

    toggle menu when clicking it

--- cpskin/menu/browser/resources/cpskin_menu.js
+++ cpskin/menu/browser/resources/cpskin_menu.js
@@ -1,24 +1,27 @@
 $( document ).ready(function() {
 
-    // Add 'activated' class to menu already loaded
-    $('#portal-globalnav li.selected:not(#portaltab-index_html) a').addClass('activated');
-
-    // Toggle between menu clicked
     var clickable_menu_selector = '#portal-globalnav li:not(#portaltab-index_html) a';
-    // Click on first level menu
     $(clickable_menu_selector).each(function(){
         $(this).click(function() {
-
-            $(clickable_menu_selector).each(function(){
-                $(this).removeClass('activated');
-            });
-            $(this).addClass('activated');
-
-            $('ul.sf-menu').each(function(){
-                $(this).hide();
-            });
+            
+            var activated = $(this).hasClass('activated');
             var menu_id = this.parentNode.id.replace('portaltab-', '');
-            jQuery('#portal-globalnav-cpskinmenu-' + menu_id).show();
+
+            if (!activated) {
+
+                $(clickable_menu_selector).each(function(){
+                    $(this).removeClass('activated');
+                });
+                $(this).addClass('activated');
+
+                $('ul.sf-menu').each(function(){
+                    $(this).hide();
+                });
+                $('#portal-globalnav-cpskinmenu-' + menu_id).show();
+            } else {
+                $(this).removeClass('activated');
+                $('#portal-globalnav-cpskinmenu-' + menu_id).hide();
+            }
             return false;
         })
     });
